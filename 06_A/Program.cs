c4f1f05c06edc972e6166dafc8886c6ff70861bd
SilviuTM <silviutudor24@gmail.com>
2023-12-07 14:28:21 +0200

    seeeeeeegs day solved

---
+++ 06_A/Program.cs
@@ -0,0 +1,44 @@
+﻿var data = File.ReadAllLines(@".\input.txt");
+
+List<int> times = new();
+List<int> dists = new();
+
+int pos = 0;
+int GetNextNumber(string text)
+{
+    while (pos < text.Length && !isDigit(text[pos])) pos++;
+    if (pos == text.Length) return -1;
+
+    int nr = 0;
+    while (pos < text.Length && isDigit(text[pos]))
+    {
+        nr = nr * 10 + (text[pos] - '0');
+        pos++;
+    }
+
+    return nr;
+}
+
+bool isDigit(char c) { return c >= '0' && c <= '9'; }
+
+// input read
+int nr = GetNextNumber(data[0]);
+while (nr != -1) { times.Add(nr); nr = GetNextNumber(data[0]); }
+
+pos = 0;
+nr = GetNextNumber(data[1]);
+while (nr != -1) { dists.Add(nr); nr = GetNextNumber(data[1]); }
+
+// problem solver
+int answer = 1;
+for (int i = 0; i < times.Count; i++)
+{
+    int victories = 0;
+    for (int time = 1; time < times[i]; time++)
+        if (time * (times[i] - time) > dists[i])
+            victories++;
+
+    answer *= victories;
+}
+
+Console.WriteLine(answer);
---
+++ 06_B/Program.cs
@@ -0,0 +1,30 @@
+﻿var data = File.ReadAllLines(@".\input.txt");
+
+string aux = "";
+long time, dist;
+bool isDigit(char c) { return c >= '0' && c <= '9'; }
+
+// input read
+foreach (char c in data[0])
+    if (isDigit(c)) aux += c;
+time = long.Parse(aux);
+
+aux = "";
+foreach (char c in data[1])
+    if (isDigit(c)) aux += c;
+dist = long.Parse(aux);
+
+Console.WriteLine("Time: " + time);
+Console.WriteLine("Dist: " + dist);
+
+// problem solver
+long answer = 0;
+for (long myTime = 0; myTime < time; myTime++)
+    if (myTime * (time - myTime) > dist) // when i find the first good time, i calculate the other end as well (time - myTime)
+                                         // then add the amount of numbers between these two (a - b + 1)
+    {
+        answer = (time - myTime) - myTime + 1;
+        break;
+    }
+
+Console.WriteLine(answer);
